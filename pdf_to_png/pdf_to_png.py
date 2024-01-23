aec4c4db04329de9cb476fdd642ac3b80ef616e7
Carl Melander <carl.melander@gmail.com>
2024-01-23 14:43:19 -0600

    Update to image output format

--- pdf_to_png/pdf_to_png.py
+++ pdf_to_png/pdf_to_png.py
@@ -1,5 +1,6 @@
 import fitz  # PyMuPDF
 from PIL import Image
+import os
 
 def convert_pdf_to_png(pdf_path, output_folder):
 
@@ -15,11 +16,11 @@ def convert_pdf_to_png(pdf_path, output_folder):
         pix = page.get_pixmap()
 
         # Save pixmap
-        with open(f'{output_folder}/page_{page_number + 1}.png', 'wb') as img_file:
+        with open(f'{output_folder}/{os.path.splitext(os.path.basename(pdf_path))[0]}_Page_{page_number + 1}.png', 'wb') as img_file:
             img_file.write(pix.tobytes('png'))
 
     pdf.close()
 
-pdf_file_path = 'C:/Users/pant/Desktop/sample.pdf'  
-output_directory = 'C:/Users/pant/Desktop/PDFtoPNG'      
+pdf_file_path = 'path/to/source.pdf'  
+output_directory = 'path/to/output_folder'      
 convert_pdf_to_png(pdf_file_path, output_directory)
